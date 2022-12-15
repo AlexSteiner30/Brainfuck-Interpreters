b9ccc8981962761a15adfac2625d4d512c987d4c
AlexSteiner30 <65250433+AlexSteiner30@users.noreply.github.com>
2022-12-15 17:10:23 +0100

    Basic Python

--- Python/interpreter.py
+++ Python/interpreter.py
@@ -1,8 +1,28 @@
-def main():
-    try:
-        pass
-    except:
-        print ("An error occured")
+program = input()
 
-if __name__ == "__main__":
-    main()
+tape = [0]
+cell_index = 0
+
+pointer = 0
+while pointer < len(program):
+	instruction = program[pointer]
+	if instruction == "+":
+		tape[cell_index] += 1
+		if tape[cell_index] == 256:
+			tape[cell_index] = 0
+
+	elif instruction == "-":
+		tape[cell_index] -= 1
+		if tape[cell_index] == -1:
+			tape[cell_index] = 255
+
+	elif instruction == "<":
+		cell_index -= 1
+
+	elif instruction == ">":
+		cell_index += 1
+
+	elif instruction == ".":
+		print(chr(tape[cell_index]), end="")
+	
+	pointer += 1
