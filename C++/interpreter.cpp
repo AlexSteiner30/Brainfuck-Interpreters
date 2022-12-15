1fea75e7fc9b806076c55013e54815e53b82dfd6
AlexSteiner30 <65250433+AlexSteiner30@users.noreply.github.com>
2022-12-15 18:15:38 +0100

    CPP Basics

--- C++/interpreter.cpp
+++ C++/interpreter.cpp
@@ -1,6 +1,60 @@
 #include <iostream>
+#include <fstream>
+#include <sstream>
 
-int main()
+int main(int argc, char *argv[])
 {
-    return 0;
+    try
+    {
+        int mem[30000]; // max bytes 
+        int memPos = 0;
+
+        std::ifstream file(argv[1]);
+        std::stringstream buffer;
+        std::string command;
+
+        buffer << file.rdbuf();
+        command = buffer.str();
+
+        int count = 0;
+
+        while (count < command.length())
+        {
+            if(command[count] == '>') { memPos += 1; }
+
+            else if(command[count] == '<') { memPos -= 1; }  
+
+            else if(command[count] == '+') { 
+                mem[memPos] += 1;
+                        
+                if(mem[memPos] > 255){
+                    mem[memPos] = 0;
+                }
+            }
+
+            else if(command[count] == '-') { 
+                mem[memPos] = 1;
+                        
+                if(mem[memPos] > 255){
+                    mem[memPos] = 0;
+                }
+            }   
+
+            else if(command[count] == '.') { std::cout << ((char)mem[memPos]); }   
+
+            else if(command[count] == ',') { 
+                char input;
+                std::cin >> input;
+
+                mem[memPos] = (int)input;
+            }  
+
+            count++; 
+        }
+    }
+
+    catch(...)
+    {
+        std::cout << "Error";
+    }
 }
