0d2085c5b1c8d0561e5ab5fb5d89b78d4c944c3d
AlexSteiner30 <65250433+AlexSteiner30@users.noreply.github.com>
2022-12-15 20:11:16 +0100

    JS Basics

--- JS/interpreter.js
+++ JS/interpreter.js
@@ -1,4 +1,56 @@
+var fs = require('fs');
+
 function main()
 {
-     
+        let mem = [] 
+        let memPos = 0;
+
+        for (var i = 0; i < 3000; i++) { // max bytes
+            mem.push(i);
+        }
+
+        let file = fs.readFileSync("brainfuck.bf");
+        var command = file.toString();
+
+        var count = 0;
+
+        while (count < command.length) {
+            if (command[count] == '>') {
+                memPos += 1;
+            }
+
+            else if (command[count] == '<') {
+                memPos -= 1;
+            }
+
+            else if (command[count] == '+') {
+                mem[memPos] += 1;
+                
+                if (mem[memPos] > 255) {
+                    mem[memPos] = 0;
+                }
+            }
+
+            else if (command[count] == '-') {
+                mem[memPos] = 1;
+
+                if (mem[memPos] > 255) {
+                    mem[memPos] = 0;
+                }
+            }
+
+            else if (command[count] == '.') {
+                console.log(String.fromCharCode(mem[memPos]));
+            }
+
+            else if (command[count] == ',') {
+                let input = prompt();
+                mem[memPos] = input;
+            }
+
+            count++;
+        }
+  
 }
+
+main();
