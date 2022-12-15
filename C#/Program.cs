57d6bb93efc51d29cdd14bf0860b4925e1747663
AlexSteiner30 <65250433+AlexSteiner30@users.noreply.github.com>
2022-12-15 16:43:46 +0100

    Project Setup

---
+++ C#/Program.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+class Interpreter
+{
+    private static void Main()
+    {
+        Console.WriteLine("Hello World!");
+    }
+}
---
+++ Python/interpreter.py
@@ -0,0 +1,8 @@
+def main():
+    try:
+        pass
+    except:
+        print ("An error occured")
+
+if __name__ == "__main__":
+    main()
