83f0eb560f9881aa500585b1e56e13dabe997a6d
AlexSteiner30 <65250433+AlexSteiner30@users.noreply.github.com>
2022-12-15 21:15:37 +0100

    Python Update

--- Python/interpreter.py
+++ Python/interpreter.py
@@ -1,21 +1,20 @@
+import sys
 
-def main():
-    try:
-        mem = [] 
+def main(*argv):
+        mem = [0] 
         memPos = 0 
-
-        for i in range (30000):
-            mem.append(i);
-
 
         count = 0
 
-        command = open('brainfuck.bf').read()
+        command = open(sys.argv[1]).read()
 
         while count < len(command):
             if (command[count] == '>'):
                 memPos += 1
 
+                if(len(mem) <= memPos + 1):
+                    mem.append(0)
+                
             elif (command[count] == '<'):
                 memPos -= 1
             
@@ -35,12 +34,16 @@ def main():
                 print(chr(mem[memPos]), end="");
 
             elif command[count] == ',':
-                input = input();
-                mem[memPos] = input;
-     
+                input = input()
+                mem[memPos] = input
+
+            elif command[count] == "[":
+               pass
+				
+            elif command[count] == "]":
+                pass
+            
             count+=1
-    except:
-        print("Error")
 
 if __name__ == "__main__":
 	main()
